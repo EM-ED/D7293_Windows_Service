de92c5ce656ad2d3d82b5e21a834634b2b4a6ce1
parsakhatibzadeh <Parsa_Khatibzadeh@outlook.com>
2023-12-05 22:30:55 +0330

    Add project files.

---
+++ CommandCenterCommunications.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace D7293_Windows_Service
+{
+    internal class CommandCenterCommunications
+    {
+        IPAddress ServerEndPoint { get; set; }
+        public CommandCenterCommunications(IPAddress ServerEndPoint_)
+        {
+            ServerEndPoint = ServerEndPoint_;
+
+        }
+
+        public async Task PostRequestAsync(Dictionary<string, string> Key_And_Values)
+        {
+
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    // Set the content type header to indicate JSON
+                    httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                    // Serialize the data to JSON
+                    string jsonBody = JsonConvert.SerializeObject(Key_And_Values);
+
+                    // Create the content to be sent
+                    var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+                    // Send the POST request
+                    await httpClient.PostAsync(ServerEndPoint.ToString(), content);
+                }
+            }
+            catch
+            {
+
+            }
+
+
+        }
+
+    }
+}
---
+++ ProcessControl.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D7293_Windows_Service
+{
+    internal class ProcessControl
+    {
+        List<string> Whitelisted_Processes_MD5;
+
+        public ProcessControl() 
+        {
+
+        }
+
+        /// <summary>
+        /// Verifies All Running Processes, Kills the ones that are netither have a valid X.509 Certificate or are part of the whitelist
+        /// </summary>
+        public void VerifyAllProcesses()
+        {
+            Process[] AllProcesses = GetAllRunningProcesses();
+            foreach (Process Process_ in AllProcesses)
+            {
+                if(ProcessHasValidCertificate(Process_) || ProcessIsWhitelisted(Process_)) { continue; }
+                else
+                {
+                    EndProcess(Process_);
+                }
+            }
+        }
+
+        #region Private Methods
+        private Process[] GetAllRunningProcesses()
+        {
+            return Process.GetProcesses();
+        }
+
+        private static bool ProcessHasValidCertificate(Process Process_)
+        {
+            try
+            {
+                string pathToExe = Process_.MainModule.FileName;
+
+                // Loads the X.509 certificate from the main EXE
+                X509Certificate2 certificate = new X509Certificate2(pathToExe);
+
+                return certificate.Verify();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool ProcessIsWhitelisted(Process Process_)
+        {
+            ///REPLACE WITH CODE COMMUNICATING WITH SERVER!
+            return false;
+        }
+        private static void EndProcess(Process Process_)
+        {
+            Process_.Kill();
+        }
+        #endregion
+    }
+}
